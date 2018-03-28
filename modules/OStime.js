5d404afae28f91e0c1f046b6826a711e37f8ead7
Bartłomiej Bajda <allerbbb@gmail.com>
2018-03-28 01:39:25 +0000

    add files

---
+++ modules/OStime.js
@@ -0,0 +1,9 @@
+function timeConvert(time) {
+    if (time <= 3600 ) {
+        return console.log("Uptime: " + Math.floor(time/60) + "min. " + time % 60 + "sek." );
+    } else if (time >= 3600) {
+        return console.log("Uptime: " + Math.floor(time/3600) + "godz. " + Math.floor((time % 3600)/60) + " min. " + (time % 60).toFixed(0) + "sek.");
+    } 
+}
+
+exports.print = timeConvert;
